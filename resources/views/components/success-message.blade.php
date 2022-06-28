339df9c36b22283570b3203a81b90fc8c192fe55
tomtomturbo <thomasreist@gmail.com>
2022-06-28 13:40:10 +0200

    first commit

---
+++ resources/views/components/success-message.blade.php
@@ -0,0 +1,15 @@
+@props(['errors'])
+
+@if ($errors->any())
+    <div {{ $attributes }}>
+        <div class="font-medium text-red-600">
+            {{ __('Success') }}
+        </div>
+
+        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+            @foreach ($errors->all() as $error)
+                <li>{{ 'message' }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
