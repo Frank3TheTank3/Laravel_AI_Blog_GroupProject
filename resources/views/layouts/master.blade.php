7f0c2eba2c626d7fe439f9b2b3ae92f21dce97cf
tomtomturbo <thomasreist@gmail.com>
2022-06-28 16:30:57 +0200

    master.blade.php created (no content yet)

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+   ...
+   <!-- Scripts -->
+   <script src="{{ asset('js/app.js') }}" defer></script>
+   <!-- Styles -->
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+   <h1>some text</h1>
+</body>
+</html>
