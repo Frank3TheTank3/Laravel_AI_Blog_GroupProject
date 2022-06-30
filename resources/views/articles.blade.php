7b24683eb56d49ebf2859e33ab988fa2fc380805
bambinaCh <chaimaa.jarite@gmail.com>
2022-06-30 15:25:25 +0200

    master blade preparation

---
+++ resources/views/articles.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+
+
+@section('content')
+<!-- hier wird der Wert von der section "content" eines blade templates ausgefüllt,
+  welches dieses layout "extended" -->
+
+<div class="content">
+   <div class="titles">
+      LISTED MOVIES
+   </div>
+   <div class="listed-movies">
+      <ul class="movies-ul">
+         @foreach ($movies as $movie)
+         <li class="movies-li">
+            <b>
+            <img class="img-cover" src="data:image/jpg;base64,{{ chunk_split(base64_encode($movie->cover))}}"></br>
+               <div class="movie-title">
+                  {{$movie->title}}
+               </div>
+            </b><br>
+         </li>
+         @endforeach
+      </ul>
+   </div>
+</div>
+@endsection
