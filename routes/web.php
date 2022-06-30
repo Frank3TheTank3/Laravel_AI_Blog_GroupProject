22fd3faf67d4150d80d5513027f90b61c3e8f258
tomtomturbo <thomasreist@gmail.com>
2022-06-30 10:15:32 +0200

    added register button to login blade

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
-
 
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/dashboard', function () {
