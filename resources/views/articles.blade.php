0385d4f1abbde522828bf6e68ea2fb7726ffa04f
bambinaCh <chaimaa.jarite@gmail.com>
2022-07-06 19:29:06 +0200

    styling article loop

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -6,24 +6,25 @@
   welches dieses layout "extended" -->
 
 <div class="content">
-   <div class="section">
-     LATEST NEWS
+   <div class="section" style="padding:2rem; font-weight:bolder; font-size:2rem;">
+      LATEST NEWS
    </div>
-   <div class="">
-      <ul class="articles">
-         @foreach ($articles as $article)
-         <li class="articles-li">
-            <b>
-               <div class="title">
-                  {{$article->post_title}}
-               </div>
-               <div class="text">
-               {{$article->post_abstract}}
-               </div>
+
+   <div class="container">
+      @foreach ($articles as $article)
+
+      <div class="row" id="article">
+         <div class="col-sm" id="title" style="color: #5e17eb; padding:2rem; font-weight:bolder; font-size:larger;">
+            {{$article->post_title}}
+         </div>
+         <div class="col-sm" id="text" style="padding:2rem;">
+            {{$article->post_abstract}}
+         </div>
+         <div class="col-sm" style="padding:2rem;">
             <img class="img-cov" src="data:image/jpg;base64,{{ chunk_split(base64_encode($article->img_01))}}"></br></b><br>
-         </li>
-         @endforeach
-      </ul>
+         </div>
+      </div>
+      @endforeach
    </div>
 </div>
-@endsection
+   @endsection
