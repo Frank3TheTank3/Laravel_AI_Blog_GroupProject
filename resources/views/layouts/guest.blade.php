43eed3b1ecc2afb355d1424c48c4504c7e9f0e32
lu-meili <meili.luciana@gmail.com>
2022-07-07 09:37:37 +0200

    add logo to profile and login card

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,7 +12,8 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+        <link href="{{asset('css/mainstyles.css')}}" rel="stylesheet">
+        
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
